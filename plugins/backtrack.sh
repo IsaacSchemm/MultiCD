b9bb65e48233e7998187db02b3428436e9ad7b6a
maybeway36 <maybeway36@gmail.com>
2010-03-15 19:01:40 -0500

    Backtrack now uses /boot/backtrack instead of /casper

--- plugins/backtrack.sh
+++ plugins/backtrack.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 #BackTrack plugin for multicd.sh (designed for BackTrack 4)
-#version 5.0
-#Copyright (c) 2009 maybeway36
+#version 5.3
+#Copyright (c) 2010 maybeway36
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -23,14 +23,10 @@ set -e
 #THE SOFTWARE.
 if [ $1 = scan ];then
 	if [ -f backtrack.iso ];then
-		if [ -f ubuntu.iso ] || [ -f linuxmint.iso ];then
-			echo "Backtrack uses casper, so it can't be used with Ubuntu or Linux Mint right now. This feature will probably be added later."
-		else
-			echo "BackTrack"
-		fi
+		echo "BackTrack"
 	fi
 elif [ $1 = copy ];then
-	if [ -f backtrack.iso ] && [ ! -f ubuntu.iso ] && [ ! -f linuxmint.iso ];then
+	if [ -f backtrack.iso ];then
 		echo "Copying BackTrack..."
 		if [ ! -d backtrack ];then
 			mkdir backtrack
@@ -39,56 +35,67 @@ elif [ $1 = copy ];then
 			umount backtrack
 		fi
 		mount -o loop backtrack.iso backtrack/
-		cp -R backtrack/casper multicd-working/
-		mkdir multicd-working/boot/backtrack
+		cp -R backtrack/casper multicd-working/boot/backtrack
 		cp backtrack/boot/vmlinuz multicd-working/boot/backtrack/
 		cp backtrack/boot/initrd* multicd-working/boot/backtrack/
 		umount backtrack
 		rmdir backtrack
+		echo -n "Making initrd(s)..."
+		for i in initrd.gz initrd800.gz initrdfr.gz;do
+			mkdir ubuntu2-inittmp
+			cd ubuntu2-inittmp
+			gzip -cd ../multicd-working/boot/backtrack/$i | cpio -id
+			perl -pi -e 's/$path\/casper/$path\/boot\/backtrack/g' scripts/casper
+			perl -pi -e 's/$directory\/casper/$directory\/boot\/backtrack/g' scripts/casper
+			find . | cpio --create --format='newc' | gzip -c > ../multicd-working/boot/backtrack/$i
+			cd ..
+			rm -r ubuntu2-inittmp
+		done
+		echo " done."
 	fi
 elif [ $1 = writecfg ];then
-if [ -f backtrack.iso ] && [ ! -f ubuntu.iso ] && [ ! -f linuxmint.iso ];then
+if [ -f backtrack.iso ];then
 cat >> multicd-working/boot/isolinux/isolinux.cfg << "EOF"
 label			backtrack1024
 menu label		Start ^BackTrack FrameBuffer (1024x768)
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper nopersistent rw quiet vga=0x317
+append			BOOT=casper boot=casper nopersistent rw quiet vga=0x317 ignore_uuid
 initrd			/boot/backtrack/initrd.gz
 
 label			backtrack800
 menu label		Start BackTrack FrameBuffer (800x600)
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper nopersistent rw quiet vga=0x314
+append			BOOT=casper boot=casper nopersistent rw quiet vga=0x314 ignore_uuid
 initrd			/boot/backtrack/initrd800.gz
 
 label			backtrack-forensics
 menu label		Start BackTrack Forensics (no swap)
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper nopersistent rw vga=0x317
+append			BOOT=casper boot=casper nopersistent rw vga=0x317 ignore_uuid
 initrd			/boot/backtrack/initrdfr.gz
 
 label			backtrack-safe
 menu label 		Start BackTrack in Safe Graphical Mode
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper xforcevesa rw quiet 
+append			BOOT=casper boot=casper xforcevesa rw quiet ignore_uuid
 initrd			/boot/backtrack/initrd.gz
 
 label			backtrack-persistent
 menu label		Start Persistent Backtrack Live CD
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper persistent rw quiet 
+append			BOOT=casper boot=casper persistent rw quiet ignore_uuid
 initrd			/boot/backtrack/initrd.gz
 
 label			backtrack-text
 menu label		Start BackTrack in Text Mode
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper nopersistent textonly rw quiet
+append			BOOT=casper boot=casper nopersistent textonly rw quiet ignore_uuid
 initrd			/boot/backtrack/initrd.gz
 
 label			backtrack-ram
 menu label		Start BackTrack Graphical Mode from RAM
 kernel			/boot/backtrack/vmlinuz
-append			BOOT=casper boot=casper toram nopersistent rw quiet 
+append			BOOT=casper boot=casper toram nopersistent rw quiet ignore_uuid
 initrd			/boot/backtrack/initrd.gz
 EOF
 fi
