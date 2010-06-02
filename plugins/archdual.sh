b3cddb4dac85421f6c3bd2e4e402de69e4749ae0
maybeway36 <maybeway36@gmail.com>
2010-06-02 17:12:26 -0500

    Updated arch plugins; thanks jerome_bc

--- plugins/archdual.sh
+++ plugins/archdual.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-#Arch Linux installer plugin for multicd.sh
+#Arch Linux (dual i686/x86_64) installer plugin for multicd.sh
 #version 5.5
 #Copyright (c) 2010 maybeway36
 #Thanks to jerome_bc for updating this script for the newest Arch
@@ -23,34 +23,46 @@ set -e
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 if [ $1 = scan ];then
-	if [ -f arch.iso ];then
-		echo "Arch Linux"
+	if [ -f archdual.iso ];then
+		echo "Arch Linux Dual"
 	fi
 elif [ $1 = copy ];then
-	if [ -f arch.iso ];then
-		echo "Copying Arch Linux..."
+	if [ -f archdual.iso ];then
+		echo "Copying Arch Linux Dual..."
 		if [ ! -d arch ];then
 			mkdir arch
 		fi
 		if grep -q "`pwd`/arch" /etc/mtab ; then
 			umount arch
 		fi
-		mount -o loop arch.iso arch/
+		mount -o loop archdual.iso arch/
 		mkdir multicd-working/boot/arch
-		cp arch/boot/vmlinuz26 multicd-working/boot/arch/vmlinuz26 #Kernel
-		cp arch/boot/archiso.img multicd-working/boot/arch/archiso.img #initrd
-		cp arch/*.sqfs multicd-working/ #Compressed filesystems
+		mkdir multicd-working/boot/arch/i686
+		mkdir multicd-working/boot/arch/x86_64
+		mkdir multicd-working/i686
+		mkdir multicd-working/x86_64
+		cp arch/boot/i686/vmlinuz26 multicd-working/boot/arch/i686/vmlinuz26 #i686 Kernel
+		cp arch/boot/x86_64/vmlinuz26 multicd-working/boot/arch/x86_64/vmlinuz26 #x86_64 Kernel
+		cp arch/boot/i686/archiso.img multicd-working/boot/arch/i686/archiso.img #i686 initrd
+		cp arch/boot/x86_64/archiso.img multicd-working/boot/arch/x86_64/archiso.img #x86_64 initrd
+		cp arch/i686/*.sqfs multicd-working/i686 #i686 Compressed filesystems
+		cp arch/x86_64/*.sqfs multicd-working/x86_64 #x86_64 Compressed filesystems
 		cp arch/isomounts multicd-working/ #Text file
 		umount arch
 		rmdir arch
 	fi
 elif [ $1 = writecfg ];then
-if [ -f arch.iso ];then
+if [ -f archdual.iso ];then
 echo "
-label arch
-menu label Boot ArchLive
-kernel /boot/arch/vmlinuz26
-append lang=en locale=en_US.UTF-8 usbdelay=5 ramdisk_size=75% initrd=/boot/arch/archiso.img archisolabel=$ISOLABEL
+label arch1
+menu label Boot ArchLive i686
+kernel /boot/arch/i686/vmlinuz26
+append lang=en locale=en_US.UTF-8 usbdelay=5 ramdisk_size=75% initrd=/boot/arch/i686/archiso.img archisolabel=$ISOLABEL
+
+label arch2
+menu label Boot ArchLive x86_64
+kernel /boot/arch/x86_64/vmlinuz26
+append lang=en locale=en_US.UTF-8 usbdelay=5 ramdisk_size=75% initrd=/boot/arch/x86_64/archiso.img archisolabel=$ISOLABEL
 " >> multicd-working/boot/isolinux/isolinux.cfg
 fi
 else
