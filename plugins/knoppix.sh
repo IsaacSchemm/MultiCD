048fd408a6593c88d97c16a8275a6c37882fd733
libertyernie <maybeway36@gmail.com>
2010-09-14 17:09:01 -0500

    Knoppix plugin update

--- plugins/knoppix.sh
+++ plugins/knoppix.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 #Knoppix plugin for multicd.sh
-#version 5.0
-#Copyright (c) 2009 maybeway36
+#version 5.8
+#Copyright (c) 2010 maybeway36
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -36,10 +36,10 @@ elif [ $1 = copy ];then
 			umount knoppix
 		fi
 		mount -o loop knoppix.iso knoppix/
-		mkdir multicd-working/KNOPPIX5
-		#Compressed filesystem and docs. We have to call it KNOPPIX5 because DSL uses KNOPPIX, and if we change that DSL's installer won't work.
+		mkdir multicd-working/KNOPPIX6
+		#Compressed filesystem and docs. We have to call it KNOPPIX6 because DSL uses KNOPPIX, and if we change that DSL's installer won't work.
 		for i in $(ls knoppix/KNOPPIX*|grep -v '^KNOPPIX2$');do
-			cp -r knoppix/KNOPPIX/$i multicd-working/KNOPPIX5/
+			cp -r knoppix/KNOPPIX/$i multicd-working/KNOPPIX6/
 		done
 		mkdir -p multicd-working/boot/knoppix
 		cp knoppix/boot/isolinux/linux multicd-working/boot/knoppix/linux
@@ -48,22 +48,20 @@ elif [ $1 = copy ];then
 	fi
 elif [ $1 = writecfg ];then
 if [ -f knoppix.iso ];then
-cat >> multicd-working/boot/isolinux/isolinux.cfg << "EOF"
-LABEL knoppix
+echo "LABEL knoppix
 MENU LABEL Knoppix
 KERNEL /boot/knoppix/linux
-APPEND ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 initrd=/boot/knoppix/minirt.gz knoppix_dir=KNOPPIX5 nomce highres=off loglevel=0 libata.atapi_enabled=1 quiet SELINUX_INIT=NO nmi_watchdog=0 BOOT_IMAGE=knoppix
+APPEND ramdisk_size=100000 lang=en vt.default_utf8=0 apm=power-off vga=791 initrd=minirt.gz nomce quiet loglevel=0 tz=localtime knoppix_dir=KNOPPIX6
 
 LABEL adriane
 MENU LABEL Adriane (Knoppix)
 KERNEL /boot/knoppix/linux
-APPEND ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=785 initrd=/boot/knoppix/minirt.gz knoppix_dir=KNOPPIX5 nomce highres=off loglevel=0 libata.atapi_enabled=1 quiet SELINUX_INIT=NO nmi_watchdog=0 BOOT_IMAGE=adriane
+APPEND ramdisk_size=100000 lang=en vt.default_utf8=0 apm=power-off vga=791 initrd=minirt.gz nomce quiet loglevel=0 tz=localtime knoppix_dir=KNOPPIX6 adriane
 
 LABEL knoppix-2
 MENU LABEL Knoppix (boot to command line)
 KERNEL /boot/knoppix/linux
-APPEND ramdisk_size=100000 init=/etc/init lang=us apm=power-off vga=791 initrd=/boot/knoppix/minirt.gz knoppix_dir=KNOPPIX5 nomce highres=off loglevel=0 libata.atapi_enabled=1 quiet SELINUX_INIT=NO nmi_watchdog=0 BOOT_IMAGE=knoppix 2
-EOF
+APPEND ramdisk_size=100000 lang=en vt.default_utf8=0 apm=power-off vga=791 initrd=minirt.gz nomce quiet loglevel=0 tz=localtime knoppix_dir=KNOPPIX6 2" >> multicd-working/boot/isolinux/isolinux.cfg
 fi
 else
 	echo "Usage: $0 {scan|copy|writecfg}"
