ccd2a8faf2b66819c373b85fc7eb4106b63d3769
libertyernie <maybeway36@gmail.com>
2010-09-14 11:05:30 -0500

    Linux Mint plugin changed to use ubuntu-common

--- plugins/linuxmint.sh
+++ plugins/linuxmint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 #Linux Mint plugin for multicd.sh
-#version 5.4
+#version 5.8
 #Copyright (c) 2010 maybeway36, Zirafarafa
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,25 +28,7 @@ if [ $1 = scan ];then
 elif [ $1 = copy ];then
 	if [ -f linuxmint.iso ];then
 		echo "Copying Linux Mint..."
-		if [ ! -d linuxmint ];then
-			mkdir linuxmint
-		fi
-		if grep -q "`pwd`/linuxmint" /etc/mtab ; then
-			umount linuxmint
-		fi
-		mount -o loop linuxmint.iso linuxmint/
-		cp -R linuxmint/casper multicd-working/boot/linuxmint #Live system
-	        #if [ -d linuxmint/drivers ];then cp -R linuxmint/drivers multicd-working/;fi #These don't exist anymore as of Mint 8
-	        cp -R linuxmint/preseed multicd-working/boot/linuxmint
-	        #cp -R linuxmint/.disk multicd-working/ #The UUID isn't needed when ignore_uuid is used
-	        cp -R linuxmint/isolinux/splash.jpg multicd-working/boot/isolinux/linuxmint.jpg #A few more helper files
-		# Fix the isolinux.cfg
-		cp linuxmint/isolinux/isolinux.cfg multicd-working/boot/linuxmint/linuxmint.cfg
-		sed -i 's@file=/cdrom/preseed/@file=/cdrom/boot/linuxmint/preseed/@g' multicd-working/boot/linuxmint/linuxmint.cfg
-		sed -i 's^initrd=/casper/^live-media-path=/boot/linuxmint ignore_uuid initrd=/boot/linuxmint/^g' multicd-working/boot/linuxmint/linuxmint.cfg
-		sed -i 's^kernel /casper/^kernel /boot/linuxmint/^g' multicd-working/boot/linuxmint/linuxmint.cfg
-		sed -i 's^splash.jpg^linuxmint.jpg^g' multicd-working/boot/linuxmint/linuxmint.cfg
-		umount linuxmint;rmdir linuxmint
+		plugins/ubuntu-common linuxmint
 	fi
 elif [ $1 = writecfg ];then
 if [ -f linuxmint.iso ];then
