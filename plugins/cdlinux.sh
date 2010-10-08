4bd43bbd67746f3ff39a3c6eddf6e2bfce4f8dcf
libertyernie <maybeway36@gmail.com>
2010-10-08 16:16:19 -0500

    Caine and CDlinux plugin updates (not tested)

--- plugins/cdlinux.sh
+++ plugins/cdlinux.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
+. ./functions.sh
 #CDlinux plugin for multicd.sh
-#version 5.0
-#Copyright (c) 2010 PsynoKhi0
+#version 6.0
+#Copyright (c) 2010 PsynoKhi0, maybeway36
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -28,71 +29,82 @@ if [ $1 = scan ];then
 elif [ $1 = copy ];then
 	if [ -f cdl.iso ];then
 		echo "Copying CDlinux..."
-		if [ ! -d cdlinux ];then
-			mkdir cdlinux
-		fi
-		if grep -q "`pwd`/cdlinux" /etc/mtab ; then
-			umount cdlinux
-		fi
-		mount -o loop cdl.iso cdlinux/
-		cp -r cdlinux/CDlinux multicd-working/CDlinux #Everything in one folder
-		rm multicd-working/CDlinux/boot/memtest.bin.gz #Remove redundant memtest
-		umount cdlinux
-		rmdir cdlinux
+		mcdmount cdlinux
+		cp -r $MNT/cdlinux/CDlinux $WORK/CDlinux #Everything in one folder
+		rm $WORK/CDlinux/boot/memtest.bin.gz #Remove redundant memtest
+		umcdmount cdlinux
 	fi
 elif [ $1 = writecfg ];then
 if [ -f cdl.iso ];then
-cat >> multicd-working/boot/isolinux/isolinux.cfg << "EOF"
-#Uncomment extra language options as needed
-label cdlinux-sfg
-	menu label ^CDlinux Safe Graphics Mode
-	kernel /CDlinux/bzImage quiet CDL_SAFEG=yes
-	append initrd=/CDlinux/initrd 
-#label cdlinux-de_DE
-#	menu label ^CDlinux (de_DE) Deutsch
-#	kernel /CDlinux/bzImage quiet CDL_LANG=de_DE.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-en_CA
-#	menu label ^CDlinux (en_CA) English
-#	kernel /CDlinux/bzImage quiet CDL_LANG=en_CA.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-en_GB
-#	menu label ^CDlinux (en_GB) English
-#	kernel /CDlinux/bzImage quiet CDL_LANG=en_GB.UTF-8
-#	append initrd=/CDlinux/initrd
+#CDLinux uses country codes longer than two letters, so I don't think I'll get much out of $TAGS/lang here.
+echo "menu begin ^CDlinux Menu
+
 label cdlinux-en_US
 	menu label ^CDlinux (en_US) English
-	kernel /CDlinux/bzImage quiet CDL_LANG=en_US.UTF-8
-	append initrd=/CDlinux/initrd
-#label cdlinux-fr_CA
-#	menu label ^CDlinux (fr_CA) French
-#	kernel /CDlinux/bzImage quiet CDL_LANG=fr_CA.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-fr_CH
-#	menu label ^CDlinux (fr_CH) French
-#	kernel /CDlinux/bzImage quiet CDL_LANG=fr_CH.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-fr_FR
-#	menu label ^CDlinux (fr_FR) French
-#	kernel /CDlinux/bzImage quiet CDL_LANG=fr_FR.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-ja_JP
-#	menu label ^CDlinux (ja_JP) Japanese
-#	kernel /CDlinux/bzImage quiet CDL_LANG=ja_JP.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-ru_RU
-#	menu label ^CDlinux (ru_RU) Russian
-#	kernel /CDlinux/bzImage quiet CDL_LANG=ru_RU.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-zh_CN
-#	menu label ^CDlinux (zh_CN) Chinese
-#	kernel /CDlinux/bzImage quiet CDL_LANG=zh_CN.UTF-8
-#	append initrd=/CDlinux/initrd
-#label cdlinux-zh_TW
-#	menu label ^CDlinux (zh_TW) Chinese
-#	kernel /CDlinux/bzImage quiet CDL_LANG=zh_TW.UTF-8
-#	append initrd=/CDlinux/initrd
-EOF
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=en_US.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-de_DE
+	menu label ^CDlinux (de_DE) Deutsch
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=de_DE.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-en_CA
+	menu label ^CDlinux (en_CA) English
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=en_CA.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-en_GB
+	menu label ^CDlinux (en_GB) English
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=en_GB.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-fr_CA
+	menu label ^CDlinux (fr_CA) French
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=fr_CA.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-fr_CH
+	menu label ^CDlinux (fr_CH) French
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=fr_CH.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-fr_FR
+	menu label ^CDlinux (fr_FR) French
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=fr_FR.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-ja_JP
+	menu label ^CDlinux (ja_JP) Japanese
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=ja_JP.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-ru_RU
+	menu label ^CDlinux (ru_RU) Russian
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=ru_RU.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-zh_CN
+	menu label ^CDlinux (zh_CN) Chinese
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=zh_CN.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-zh_TW
+	menu label ^CDlinux (zh_TW) Chinese
+	kernel /CDlinux/bzImage
+	append quiet CDL_LANG=zh_TW.UTF-8
+	initrd /CDlinux/initrd
+label cdlinux-sfg
+	menu label ^CDlinux Safe Graphics Mode
+	kernel /CDlinux/bzImage
+	append quiet CDL_SAFEG=yes
+	initrd /CDlinux/initrd 
+label back
+	menu label Back to Main Menu
+	com32 menu.c32
+	append isolinux.cfg
+menu end
+" >> multicd-working/boot/isolinux/isolinux.cfg
 fi
 else
 	echo "Usage: $0 {scan|copy|writecfg}"
