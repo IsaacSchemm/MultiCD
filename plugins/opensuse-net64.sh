91d695d0e8ed6eb4a2450a9ccc26ce8822068832
libertyernie <maybeway36@gmail.com>
2010-11-19 10:36:51 -0600

    fix

--- plugins/opensuse-net64.sh
+++ plugins/opensuse-net64.sh
@@ -22,7 +22,9 @@ set -e
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
-if [ $1 = scan ];then
+if [ $1 = links ];then
+	echo "openSUSE-*-NET-x86_64.iso opensuse-net64.iso"
+elif [ $1 = scan ];then
 	if [ -f opensuse-net64.iso ];then
 		echo "openSUSE netboot installer"
 	fi
