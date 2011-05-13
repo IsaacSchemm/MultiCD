c036a6a8d7e3482cfb0cc880f33f13b73225df8e
Isaac Schemm <isaacschemm@gmail.com>
2011-05-13 05:27:23 -0500

    caine fix - thanks to iomari (ubuntuforums)

--- plugins/caine.sh
+++ plugins/caine.sh
@@ -23,46 +23,36 @@ set -e
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 if [ $1 = scan ];then
-	if [ -f caine.iso ];then
-		echo "Caine [note: this is old; it doesn't work for new versions]"
-	fi
+    if [ -f caine.iso ];then
+        echo "Caine"
+    fi
 elif [ $1 = copy ];then
-	if [ -f caine.iso ];then
-		echo "Copying Caine..."
-		mcdmount caine
-		cp -R $MNT/caine/casper $WORK/boot/caine #Live system
-		cp $MNT/caine/README.diskdefines multicd-working/
-		mkdir $WORK/CaineFiles
-		cp -R $MNT/caine/{AutoPlay,autorun.exe,autorun.inf,comdlg32.ocx,files,license.txt,page5,preseed,Programs,RegOcx4Vista.bat,rw_common,tabctl32.ocx,vbrun60.exe,WinTaylor.exe} $WORK/CaineFiles
-		umcdmount caine
-		echo -n "Making initrd(s)..." #This initrd code is common to distros using old versions of casper
-		WORKPATH="$(readlink -f "$WORK")"
-		if [ -d $MNT/caine-inittmp ];then rm -r $MNT/caine-inittmp;fi
-		mkdir $MNT/caine-inittmp
-		cd $MNT/caine-inittmp
-		gzip -cd $WORKPATH/boot/caine/initrd.gz | cpio -id
-		perl -pi -e 's/path\/casper/path\/boot\/caine/g' scripts/casper
-		perl -pi -e 's/directory\/casper/directory\/boot\/caine/g' scripts/casper
-		find . | cpio --create --format='newc' | gzip -c > $WORKPATH/boot/caine/initrd.gz
-		cd -
-		rm -r $MNT/caine-inittmp
-		echo " done."
-	fi
+    if [ -f caine.iso ];then
+        echo "Copying Caine..."
+        mcdmount caine
+        cp -R $MNT/caine/casper $WORK/boot/caine #Live system
+        cp $MNT/caine/README.diskdefines multicd-working/
+        mkdir $WORK/CaineFiles
+        for item in AutoPlay autorun.exe autorun.inf comdlg32.ocx files license.txt page5 preseed Programs RegOcx4Vista.bat rw_common tabctl32.ocx vbrun60.exe WinTaylor.exe; do
+            [[ -a $MNT/caine/$item ]] && cp -R $MNT/caine/$item $WORK/CaineFiles
+        done
+        umcdmount caine
+    fi
 elif [ $1 = writecfg ];then
-	if [ -f $TAGS/lang ];then
-		LANGCODE=$(cat $TAGS/lang)
-	else
-		LANGCODE=en
-	fi
-	if [ -f caine.iso ];then
-		echo "label caine2
-		kernel /boot/caine/vmlinuz
-		initrd /boot/caine/initrd.gz
-		append live-media-path=/boot/caine ignore_uuid noprompt persistent BOOT_IMAGE=/casper/vmlinuz file=/cdrom/CaineFiles/custom.seed boot=casper -- debian-installer/language=$LANGCODE console-setup/layoutcode=$(cat $TAGS/lang)
-		" >> $WORK/boot/isolinux/isolinux.cfg
-	fi
+    if [ -f $TAGS/lang ];then
+        LANGCODE=$(cat $TAGS/lang)
+    else
+        LANGCODE=en
+    fi
+    if [ -f caine.iso ];then
+        echo "label caine2 (Computer Aided Investigative Environment)
+        kernel /boot/caine/vmlinuz
+        initrd /boot/caine/initrd.gz
+        append live-media-path=/boot/caine ignore_uuid noprompt persistent BOOT_IMAGE=/casper/vmlinuz file=/cdrom/CaineFiles/custom.seed boot=casper -- debian-installer/language=$LANGCODE console-setup/layoutcode=$LANGCODE
+        " >> $WORK/boot/isolinux/isolinux.cfg
+    fi
 else
-	echo "Usage: $0 {scan|copy|writecfg}"
-	echo "Use only from within multicd.sh or a compatible script!"
-	echo "Don't use this plugin script on its own!"
+    echo "Usage: $0 {scan|copy|writecfg}"
+    echo "Use only from within multicd.sh or a compatible script!"
+    echo "Don't use this plugin script on its own!"
 fi
