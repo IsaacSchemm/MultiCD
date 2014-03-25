093b1300672a0af0dc071202eec91ab429717879
Isaac Schemm <isaacschemm@gmail.com>
2014-03-25 17:33:47 -0500

    Update tiny core plugin (add boot menu options)

--- plugins/tinycore.sh
+++ plugins/tinycore.sh
@@ -2,8 +2,8 @@
 set -e
 . "${MCDDIR}"/functions.sh
 #Tiny Core Linux (also Core, CorePlus) plugin for multicd.sh
-#version 7.1
-#Copyright (c) 2012 Isaac Schemm
+#version 20140325
+#Copyright (c) 2014 Isaac Schemm
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -22,27 +22,9 @@ set -e
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
-tinycorecommon () {
-if [ ! -z "$1" ] && [ -f $1.iso ];then
-mcdmount $1
-mkdir "${WORK}"/boot/tinycore
-cp "${MNT}"/$1/boot/vmlinuz "${WORK}"/boot/tinycore/vmlinuz #Linux kernel - 4.0 or newer
-cp "${MNT}"/$1/boot/*.gz "${WORK}"/boot/tinycore/ #Copy any initrd there may be - this works for (old) microcore too
-if [ -d "${MNT}"/$1/tce ];then
-cp -r "${MNT}"/$1/tce "${WORK}"/
-fi
-if [ -d "${MNT}"/$1/cde ];then
-cp -r "${MNT}"/$1/cde "${WORK}"/
-fi
-sleep 1
-umcdmount $1
-else
-echo "$0: \"$1\" is empty or not an ISO"
-exit 1
-fi
-}
+
 if [ $1 = links ];then
-	#Only one will be included
+	# Although three aliases are defined, only one ISO will be used.
 	echo "CorePlus-*.iso tinycore.iso none"
 	echo "TinyCore-*.iso tinycore.iso none"
 	echo "Core-*.iso tinycore.iso none"
@@ -53,41 +35,74 @@ elif [ $1 = scan ];then
 elif [ $1 = copy ];then
 	if [ -f tinycore.iso ];then
 		echo "Copying Tiny Core..."
-		tinycorecommon tinycore
-		#--------------------#
-		for i in `ls -1 *.tcz 2> /dev/null;true`;do
-			mkdir -p "${WORK}"/cde
-			echo "Copying: $i"
-			cp $i "${WORK}"/cde/optional/"$i"
-		done
-		#regenerate onboot.lst
-		true > "${WORK}"/cde/onboot.lst
-		for i in "${WORK}"/cde/optional/*;do
-			echo $(basename "$i") >> "${WORK}"/cde/onboot.lst
-		done
+		if [ -f tinycore.iso ];then
+			mcdmount tinycore
+			mkdir "${WORK}"/boot/tinycore
+			cp "${MNT}"/tinycore/boot/vmlinuz "${WORK}"/boot/tinycore/vmlinuz #Linux kernel - 4.0 or newer
+			cp "${MNT}"/tinycore/boot/core.gz "${WORK}"/boot/tinycore/core.gz
+			cp -r "${MNT}"/tinycore/cde "${WORK}"/
+			umcdmount tinycore
+			for i in `ls -1 *.tcz 2> /dev/null;true`;do
+				mkdir -p "${WORK}"/cde
+				echo "Copying: $i"
+				cp $i "${WORK}"/cde/optional/"$i"
+			done
+			#regenerate onboot.lst
+			true > "${WORK}"/cde/onboot.lst
+			for i in "${WORK}"/cde/optional/*;do
+				echo $(basename "$i") >> "${WORK}"/cde/onboot.lst
+			done
+		else
+			echo "$0: \"$1\" is empty or not an ISO"
+			exit 1
+		fi
 	fi
 elif [ $1 = writecfg ];then
-#BEGIN TINY CORE ENTRY#
-if [ -f tinycore.iso ];then
-	if [ -f "${TAGS}"/tinycore.name ] && [ "$(cat "${TAGS}"/tinycore.name)" != "" ];then
-		TCNAME=$(cat "${TAGS}"/tinycore.name)
-	elif [ -f tinycore.defaultname ] && [ "$(cat tinycore.defaultname)" != "" ];then
-		TCNAME=$(cat tinycore.defaultname)
-	else
-		TCNAME="Tiny Core Linux"
+	if [ -f tinycore.iso ];then
+		TCVER=""
+		if [ -f tinycore.version ] && [ "$(cat tinycore.version)" != "" ];then
+			TCVER="$(cat tinycore.version)"
+		fi
+		echo "LABEL tc
+		MENU LABEL Boot TinyCore $TCVER
+		TEXT HELP
+		Boot TinyCore with extensions loaded from the CDE folder.
+		Boot media is removable. Use TAB to edit options.
+		ENDTEXT
+		KERNEL /boot/tinycore/vmlinuz
+		INITRD /boot/tinycore/core.gz
+		APPEND loglevel=3 cde
+
+		LABEL tcw
+		MENU LABEL Boot TinyCore (on slow devices, waitusb=5)
+		TEXT HELP
+		Boot TinyCore with CDE extensions, if using a slow device.
+		Boot media is removable. Use TAB to edit options.
+		ENDTEXT
+		KERNEL /boot/tinycore/vmlinuz
+		INITRD /boot/tinycore/core.gz
+		APPEND loglevel=3 cde waitusb=5
+
+		LABEL core
+		MENU LABEL Boot Core (command line only).
+		TEXT HELP
+		No embedded X/GUI extensions are loaded. User extensions if any
+		will be loaded, and will need to provide X/GUI if required.
+		ENDTEXT
+		KERNEL /boot/tinycore/vmlinuz
+		INITRD /boot/tinycore/core.gz
+		APPEND loglevel=3
+
+		LABEL corew
+		MENU LABEL Boot Core (command line only on slow devices, waitusb=5)
+		TEXT HELP
+		No embedded X/GUI extensions are loaded. User extensions if any
+		will be loaded, and will need to provide X/GUI if required.
+		ENDTEXT
+		KERNEL /boot/tinycore/vmlinuz
+		INITRD /boot/tinycore/core.gz
+		APPEND loglevel=3 waitusb=5" >> "${WORK}"/boot/isolinux/isolinux.cfg
 	fi
-	if [ -f tinycore.version ] && [ "$(cat tinycore.version)" != "" ];then
-		TCNAME="$TCNAME $(cat tinycore.version)"
-	fi
-	for i in $(ls "${WORK}"/boot/tinycore|grep '\.gz');do
-		echo "label tinycore-$i
-		menu label ^$TCNAME ($(basename $i))
-		kernel /boot/tinycore/vmlinuz
-		append quiet cde showapps
-		initrd /boot/tinycore/$(basename $i)">>"${WORK}"/boot/isolinux/isolinux.cfg
-	done
-fi
-#END TINY CORE ENTRY#
 else
 	echo "Usage: $0 {links|scan|copy|writecfg}"
 	echo "Use only from within multicd.sh or a compatible script!"
