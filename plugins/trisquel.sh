f6040c26f7d03fe9a0a0937c7f22585a473559a2
Isaac Schemm <isaacschemm@gmail.com>
2015-04-05 19:42:49 -0500

    Add trisquel.iso alias

--- plugins/trisquel.sh
+++ plugins/trisquel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if [ $1 = links ];then
+	echo "trisquel.iso trisquel.ubuntu.iso Trisquel"
 	echo "trisquel_*.iso trisquel.ubuntu.iso Trisquel_(*)"
 	echo "trisquel-*.iso trisquel.ubuntu.iso Trisquel_(*)"
 fi
