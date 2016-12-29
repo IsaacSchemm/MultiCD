32eac6a3fdeaa21476c02d2a87894fa35351020e
Isaac Schemm <isaacschemm@gmail.com>
2016-12-29 09:55:14 -0600

    Update all plugins using ubuntucommon (except ubuntu.sh) to redirect to ubuntu.sh

--- plugins/kduxp.sh
+++ plugins/kduxp.sh
@@ -2,56 +2,7 @@
 set -e
 . "${MCDDIR}"/functions.sh
 #KDuXP plugin for multicd.sh
-#version 6.9
-#Copyright (c) 2011 Isaac Schemm
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
-
+#version 20161229
 if [ $1 = links ];then
-	echo "KDuXPv*.iso kduxp.iso none"
-elif [ $1 = scan ];then
-	if [ -f kduxp.iso ];then
-		echo "KDuXP"
-	fi
-elif [ $1 = copy ];then
-	if [ -f kduxp.iso ];then
-		echo "Copying KDuXP..."
-		ubuntucommon kduxp
-	fi
-elif [ $1 = writecfg ];then
-	 if [ -f kduxp.iso ];then
-		if [ -f kduxp.version ];then
-			KDUVER=" $(cat kduxp.version)"
-			if [ "$KDUVER" == "" ];then
-				KDUVER=""
-			fi
-		else
-			KDUVER=""
-		fi
-		echo "label kduxp
-		menu label --> KDuXP$KDUVER Menu
-		com32 menu.c32
-		append /boot/kduxp/kduxp.cfg
-		" >> "${WORK}"/boot/isolinux/isolinux.cfg
-	fi
-else
-	echo "Usage: $0 {links|scan|copy|writecfg}"
-	echo "Use only from within multicd.sh or a compatible script!"
-	echo "Don't use this plugin script on its own!"
+	echo "KDuXPv*.iso kduxp.ubuntu.iso KDuXP"
 fi
--- plugins/redobackup.sh
+++ plugins/redobackup.sh
@@ -2,50 +2,7 @@
 set -e
 . "${MCDDIR}"/functions.sh
 #Redo Backup plugin for multicd.sh
-#version 6.9
-#Copyright (c) 2011 Isaac Schemm
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
-
+#version 20161229
 if [ $1 = links ];then
-	echo "redobackup-livecd-*.iso redobackup.iso none"
-elif [ $1 = scan ];then
-	if [ -f redobackup.iso ];then
-		echo "Redo Backup"
-	fi
-elif [ $1 = copy ];then
-	if [ -f redobackup.iso ];then
-		echo "Copying Redo Backup OS..."
-		ubuntucommon redobackup
-	fi
-elif [ $1 = writecfg ];then
-	if [ -f redobackup.iso ];then
-		BASENAME=redobackup
-
-		echo "label redobackup
-		menu label --> ^Redo Backup$(getVersion) Menu
-		com32 vesamenu.c32
-		append /boot/$BASENAME/$BASENAME.cfg
-		" >> "${WORK}"/boot/isolinux/isolinux.cfg
-	fi
-else
-	echo "Usage: $0 {links|scan|copy|writecfg}"
-	echo "Use only from within multicd.sh or a compatible script!"
-	echo "Don't use this plugin script on its own!"
+	echo "redobackup-livecd-*.iso redobackup.ubuntu.iso Redo_Backup_(*)"
 fi
