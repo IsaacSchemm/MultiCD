7f2e472b015fda34d89f2aaded31e3ad6422714b
Isaac Schemm <isaacschemm@gmail.com>
2017-05-26 13:22:36 -0500

    Fix issue #53 (Manjaro 17.0.1)

--- plugins/manjaro.sh
+++ plugins/manjaro.sh
@@ -2,8 +2,8 @@
 set -e
 . "${MCDDIR}"/functions.sh
 #Manjaro plugin for multicd.sh
-#version 20161005
-#Copyright (c) 2016 Isaac Schemm
+#version 20170526
+#Copyright (c) 2017 Isaac Schemm
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -38,21 +38,45 @@ elif [ $1 = copy ];then
 			exit 1
 		fi
 		mcdcp -r "${MNT}"/manjaro/manjaro "${WORK}"
-		cat "${MNT}"/manjaro/isolinux/isolinux.cfg | sed -e 's/^ui.*//g' -e 's/isolinux\.msg/manjaro\.msg/g' -e "s/misolabel=[^ ]*/misolabel=$CDLABEL/g" > "${WORK}"/boot/isolinux/manjaro.cfg
-		cat "${MNT}"/manjaro/isolinux/isolinux.msg | sed -e '/^hdt/d' -e '/^harddisk.*/d' -e '/^memtest/d' > "${WORK}"/boot/isolinux/manjaro.msg
+		if [ -f "${MNT}"/manjaro/boot/grub/i386-pc/eltorito.img ];then
+			# Build a miniature ISO with GRUB and the kernel and initrd, and load it with memdisk. The rest of the OS will be loaded once the kernel boots.
+			SMALLISODIR=/tmp/manjaro-boot-$(date +%s)
+			mkdir $SMALLISODIR
+			mcdcp -r "${MNT}"/manjaro/boot $SMALLISODIR
+			cp $SMALLISODIR/boot/grub/kernels.cfg old.cfg
+			sed -i -e "s/misolabel=[^ ]*/misolabel=$CDLABEL/g" $SMALLISODIR/boot/grub/kernels.cfg
+			cp $SMALLISODIR/boot/grub/kernels.cfg new.cfg
+			GENERATOR=genisoimage
+			if ! which genisoimage && which mkisofs;then
+				GENERATOR=mkisofs
+			fi
+			$GENERATOR -R -b boot/grub/i386-pc/eltorito.img -no-emul-boot -boot-load-size 4 -boot-info-table -c boot.catalog -o "${WORK}"/manjaro/multicd-boot.iso $SMALLISODIR
+			rm -r $SMALLISODIR
+		else
+			cat "${MNT}"/manjaro/isolinux/isolinux.cfg | sed -e 's/^ui.*//g' -e 's/isolinux\.msg/manjaro\.msg/g' -e "s/misolabel=[^ ]*/misolabel=$CDLABEL/g" > "${WORK}"/boot/isolinux/manjaro.cfg
+			cat "${MNT}"/manjaro/isolinux/isolinux.msg | sed -e '/^hdt/d' -e '/^harddisk.*/d' -e '/^memtest/d' > "${WORK}"/boot/isolinux/manjaro.msg
+		fi
 		touch "${WORK}"/.miso
 		umcdmount manjaro
 	fi
 elif [ $1 = writecfg ];then
 	if [ -f manjaro.iso ];then
-		echo "label Manjaro
-			menu label >> ^Manjaro
-			config /boot/isolinux/manjaro.cfg" >> "${WORK}"/boot/isolinux/isolinux.cfg
+		if [ -f "${WORK}"/manjaro/multicd-boot.iso ];then
+			echo "label Manjaro
+				menu label ^Manjaro
+				kernel memdisk
+				append iso
+				initrd /manjaro/multicd-boot.iso" >> "${WORK}"/boot/isolinux/isolinux.cfg
+		else
+			echo "label Manjaro
+				menu label >> ^Manjaro
+				config /boot/isolinux/manjaro.cfg" >> "${WORK}"/boot/isolinux/isolinux.cfg
 
-			echo "label back
-			menu label Back to main menu
-			config /boot/isolinux/isolinux.cfg /boot/isolinux/
-			" >> "${WORK}"/boot/isolinux/manjaro.cfg
+				echo "label back
+				menu label Back to main menu
+				config /boot/isolinux/isolinux.cfg /boot/isolinux/
+				" >> "${WORK}"/boot/isolinux/manjaro.cfg
+		fi
 	fi
 else
 	echo "Usage: $0 {links|scan|copy|writecfg}"
